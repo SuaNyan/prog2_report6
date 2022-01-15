4c41a59a40ad9196d247d5bdb20babd3540e7bb5
Yuuna Takazato <e215724@ie.u-ryukyu.ac.jp>
2022-01-16 04:34:40 +0900

    gamemasterメソッドに手札と目標値の出力を追加

--- app/src/main/java/jp/ac/uryukyu/ie/e215724/GameMaster.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e215724/GameMaster.java
@@ -7,6 +7,7 @@ public class GameMaster {
         System.out.println("名前を入力してください");
         String playerName = scanner.next();
         System.out.println("ようこそ"+playerName+"さん。ゲームを開始します");
-        
+        System.out.println("手札は"+Player.card()+"です");
+        System.out.println("目標値　"+Player.target()+"　にしてください");
     }
 }
